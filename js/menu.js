30b053f438d44665c5258beaa70cbdcf74c270ba
Minechan-fluff18 <61097933+Minechan-fluff18@users.noreply.github.com>
2024-06-14 19:24:24 +0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $('.header__burger').click(function(event) {
-    $('.header__burger, .header__menu').toggleClass('active');
+    $('.header__burger, .header__menu, .header__list, .overlay').toggleClass('active');
     $('body').toggleClass('lock');
-  })
-})
+  });
+
+  $('.overlay').click(function() {
+    $('.header__burger, .header__menu, .header__list, .overlay').removeClass('active');
+    $('body').removeClass('lock');
+  });
+});
